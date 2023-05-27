d7e4b9c22db5451c80d2e36e73e5e9c246f954f3
anandkishorgupta <anandgupta20588@gmail.com>
2023-05-27 14:26:30 +0545

    starting socket.io

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,11 @@ import { createServer } from "http";
 import { Server } from "socket.io";
 import cors from "cors";
 import dotenv from "dotenv";
-import userRouter from "./routes/userRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import chatRoute from "./routes/chatRoutes.js";
 import connectDB from "./database/connectDB.js";
 import cookieParser from "cookie-parser";
+import Message from "./model/MessageModel.js";
 dotenv.config();
 const app = express();
 const rooms = ["general", "tech", "finance", "crypto"];
@@ -16,8 +18,8 @@ app.use(express.json());
 // app.use(cors());    
 app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 app.use(cookieParser())              
-app.use("/users", userRouter); // Mount the userRouter at the root path ("/")
-
+app.use("/users", userRoutes); // Mount the userRouter at the root path ("/")
+app.use("/api/chat",chatRoute)
 // Create HTTP server
 const httpServer = createServer(app);
 
@@ -28,11 +30,23 @@ const io = new Server(httpServer, {
     methods: ["GET", "POST"],
   },
 });
+async function getLastMessageFromRoom(room){
+  let roomMessages=await Message.aggregate([
+    {$match:{to:room}},
+    {$group:{_id:'$date',MessagesByDate:{$push:'$$ROOT'}}}
+  ])
+  return roomMessages
+}
+function sortRoomMessagesByDate
 
 // Socket.io event handling
 io.on("connection", (socket) => {
   // Handle socket.io events
   console.log("A client has connected.");
+  socket.on("join-room",async(room)=>{
+    socket.join(room);
+    let roomMessages=await getLastMessageFromRoom(room)
+  })
 });
 
 // Express routes
